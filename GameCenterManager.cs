63892bfec0d64e8daf4135b91e274de8a72a1135
halterdev <halterdev@gmail.com>
2014-04-05 15:38:58 -0400

    * gamecenter.png: gamecenter default pic
    * gamecenterlarge.png: gamecenter large pic
    
    * GameCenterManager.cs: manages gamecenter actions
    
    * Hooked.csproj: added gamecenter button images
    
    * Info.plist: enabled gamecenter

---
+++ GameCenterManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using MonoTouch.Foundation;
+using MonoTouch.UIKit;
+using MonoTouch.GameKit;
+
+namespace Hooked
+{
+	public class GameCenterManager
+	{
+		NSMutableDictionary earnedAchievementCache;
+
+		public GameCenterManager ()
+		{
+		}
+
+		public static bool isGameCenterAvailable()
+		{
+			return UIDevice.CurrentDevice.CheckSystemVersion (4, 1);
+		}
+
+		public GKLeaderboard reloadLeaderboard(string category)
+		{
+			GKLeaderboard leaderboard = new GKLeaderboard ();
+			leaderboard.Category = category;
+			leaderboard.TimeScope = GKLeaderboardTimeScope.AllTime;
+			leaderboard.Range = new NSRange (1, 1);
+			return leaderboard;
+
+		}
+
+		public void reportScore(long score, string category, MTGKTapperViewController controller)
+		{
+			GKScore scoreReporter = new GKScore (category);
+			scoreReporter.Value = score;
+			scoreReporter.ReportScore (new GKNotificationHandler ((error) => {
+				if(error == null){
+					new UIAlertView ("Score reported", "Score Reported successfully", null, "OK", null).Show ();
+				}
+				else{
+					new UIAlertView ("Score Reported Failed", "Score Reported Failed", null, "OK", null).Show ();
+				}
+				NSThread.SleepFor(1);
+				controller.updateHighScore();
+			}));
+		}
+	}
+}
+
